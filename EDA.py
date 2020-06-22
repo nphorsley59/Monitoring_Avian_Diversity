12eee999abdef32292b158d1fc13e62f6c64c351
nphorsley59 <nphorsley59@gmail.com>
2020-06-21 20:15:09 -0500

    cleaned sp 2020 data

--- EDA.py
+++ EDA.py
@@ -4,85 +4,148 @@ Spyder Editor
 
 This is a temporary script file.
 """
+## LIBRARIES
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-## IMPORT DATA
-data = pd.read_csv("Spring2020.csv")
+## IMPORT
+
+birds = pd.read_csv("Countdata.csv")
 codes = pd.read_csv("SpeciesCodes.csv")
 
-data.columns
-data.head()
+## FUNCTIONS
 
-## CLEAN DATA
-# clean up false NAs
-data = data.dropna(subset=['SpeciesCode'])
-data = data.reset_index(drop=True)
-data = data.fillna(value={'Visual':0, 'Sex':'U', 'Migrating':0, 'ClusterSize':1, 'StartTime':'00:00'})
-data['Visual'].replace('X', 1, inplace=True)
+def valueCount(column):
+    x = column.value_counts()
+    return x
 
-# NA report
-total_na = data.isna().sum().sort_values(ascending=False)
-percent_na = (data.isna().sum()*100/len(data)).sort_values(ascending=False)
-data_na = pd.concat([total_na, percent_na], axis=1, keys=['Total', 'Percent'])
+def fillDown(data, column, row):
+    if pd.isna(data[column][row]) and data['Point'][row] != '88':
+        data.at[row, column]=data.at[row-1, column]
+    else:
+        pass
 
-# investigate problematic NAs and typos
-for col in ['Distance', 'Minute', 'Point']:
-    print(pd.isnull(data[col]).to_numpy().nonzero()[0])
-data.loc[267, 'Point'] = 88
-data.loc[267, 'Minute'] = float("NaN")
+## VIEW
 
-# pull out 88s
-occurence_dets = data[data.Point == "88"]
-data = data[data.Point != "88"]
-data = data.reset_index(drop=True)
+birds.columns
+# 'ObserverID', 'Year', 'Month', 'Day', 'SurveyID', 'StartTime', 
+# 'Point', 'Minute', 'SpeciesCode', 'Distance', 'How', 'Visual', 
+# 'Sex', 'Migrating', 'ClusterSize', 'ClusterCode', 'Notes'
+# make 'Start Time' 'StartTime'
 
-# clean up time
-minutes = data['Start Time'].apply(lambda x: str(x).split(':')[1]).astype(int)
-hours = data['Start Time'].apply(lambda x: str(x).split(':')[0]).astype(int)
+for header in list(birds.columns):
+    print(valueCount(birds[header]))
+# Exceptions Handled 6/21/2020:
+# change 'D & Q' to 'DQ' (SurveyID)
+# fix '0:00' and '5^' (Point)
+# fix '88.0' (Minute)
+# fix 'C ' (Distance)
+# fix 'C' (How)
+# fix 'M', 'F', and 'J' (Visual)
+# investigate inputs (Migrating)
+# investigate inputs (ClusterSize)
+# change dtype (ClusterSize)
+
+birds.isna().sum().sort_values(ascending=False)
+# NA's Handled 6/21/2020:
+# make Y/N (Migrating, Visual)
+# delete empty lines
+# fill in known nans ()
+
+## CLEAN
+
+# make 'Start Time' 'StartTime'
+birds.rename(columns={'Start Time':'StartTime'}, inplace=True)
+
+# change 'D & Q' to 'DQ' (SurveyID)
+birds['SurveyID'].replace('D & Q', 'DQ', inplace=True)
+
+# fix '0:00' and '5^' (Point)
+birds.loc[birds['Point']=='0:00'] # 743
+birds.at[743, 'Point']='5'
+birds.loc[birds['Point']=='5^'] # 744
+birds.at[744, 'Point']='5'
+
+# fix '88.0' (Minute)
+birds.loc[birds['Minute']==88] # 271
+birds.at[271, 'Minute']=float('NaN')
+birds.at[271, 'Point']="88"
+
+# fix 'C ' (Distance)
+birds.loc[birds['Distance']=='C '] # 522
+birds.at[522, 'Distance']=float('NaN')
+birds.at[522, 'How']='C'
+
+# fix 'C' (How)
+birds.loc[birds['How']=='C '] # 812
+birds.at[812, 'How']='C'
+
+# fix 'M', 'F', and 'J' (Visual)
+birds.loc[birds['Visual']=='M'] # multiple
+birds.loc[birds['Visual']=='F'] # multiple
+birds.loc[birds['Visual']=='J'] # 768
+for letter in ['M', 'F', 'J']:
+    rows = birds.loc[birds['Visual']==letter].index.to_numpy()
+    for index in rows:
+        birds.at[index, 'Visual']=1
+        birds.at[index, 'Sex']=letter
+
+# investigate inputs (Migrating)
+for number in ['2', '4', '3', '5', '20', '40', '1', '6']:
+    rows = birds.loc[birds['Migrating']==number].index.to_numpy()
+    for index in rows:
+        birds.at[index, 'Migrating']=0
+        birds.at[index, 'ClusterCode']=birds.at[index, 'ClusterSize']
+        birds.at[index, 'ClusterSize']=number
+
+# investigate inputs (ClusterSize)
+birds.loc[birds['ClusterSize']=='G'] # multiple
+birds.loc[birds['ClusterSize']=='H'] # multiple
+for letter in ['G', 'H']:
+    rows = birds.loc[birds['ClusterSize']==letter].index.to_numpy()
+    for index in rows:
+        birds.at[index, 'ClusterSize']=1
+        birds.at[index, 'ClusterCode']=letter
+
+# change dtype (ClusterSize, Distance)
+birds['ClusterSize'] = birds['ClusterSize'].astype('float64')
+birds['Distance'] = birds['Distance'].astype('float64')
+
+# make Y/N (Migrating, Visual)
+birds = birds.fillna(value={'Migrating':0, 'Visual':0, 'ClusterSize':1, 'Sex':'U'})
+birds['Visual'].replace('X', 1, inplace=True)
+birds['Migrating'].replace('X', 1, inplace=True)
+
+# delete empty lines
+birds = birds.dropna(subset=['SpeciesCode'])
+birds = birds.reset_index(drop=True) 
+
+# fill in known nans (How, Minute, Distance, StartTime)
+np.where(birds['How'].isnull())[0] # 600, 624, 676, 704, 777, 778, 1029, 1241, 1289
+for number in [600, 624, 676, 704, 777, 778, 1029, 1241, 1289]:
+    birds.at[number, 'How']='S'
+np.where(birds['Minute'].isnull())[0] # 88s
+np.where(birds['Distance'].isnull())[0] # 600, 624, 676, 1029, 1241, 1289
+for number in [600, 624, 676, 1029, 1241, 1289]:
+    birds.at[number, 'Distance']=round(birds['Distance'].mean(), 0)
+for row in range(len(birds)):
+    fillDown(birds, 'StartTime', row)
+
+# fill in 88s
+birds = birds.fillna(value={'StartTime':'00:00', 'Minute':88, 'Distance':round(birds['Distance'].mean(), 0)})
+
+## FEATURE ENGINEERING
+
+# normalize time
+minutes = birds['StartTime'].apply(lambda x: str(x).split(':')[1]).astype(int)
+hours = birds['StartTime'].apply(lambda x: str(x).split(':')[0]).astype(int)
 minutes = minutes.apply(lambda x: round(x/60, 2))
-data['Start Time'] = hours+minutes
+birds['StartTime'] = hours+minutes
 
-# add species names
-
-## EXPLORATORY PLOTS
-# abundance plot
-abun = data.SpeciesCode.value_counts()
-ax,f = plt.subplots(figsize=(16,10))
-sns.barplot(x=abun.index, y=abun)
-plt.yticks(np.arange(0,310,10))
-plt.xticks(rotation=90)
-plt.ylabel("Abundance")
-plt.xlabel("Species")
-
-# distance plot
-dist = data.Distance
-ax,f = plt.subplots(figsize=(16,10))
-sns.distplot(dist)
-plt.xticks(np.arange(0,1225,25), rotation=90)
-
-# species by distance plot
-dist_grouped = data.groupby('SpeciesCode', as_index=False)['Distance'].mean()
-sorted_index = dist_grouped.sort_values(by=['Distance'], axis=0, ascending=False).index
-data_sorted=dist_grouped.reindex(sorted_index)
-data_sorted=data_sorted.reindex(sorted_index)
-ax,f = plt.subplots(figsize=(16,10))
-sns.barplot(x=data_sorted.SpeciesCode, y=data_sorted.Distance)
-plt.xticks(rotation=90)
-plt.yticks(np.arange(0,1050,50))
-plt.ylabel("Distance (mean)")
-plt.xlabel("Species")
-
-# detections over time
-ax,f = plt.subplots()
-sns.distplot(data.StartTime)
-plt.xlim([4.5,9.5])
-plt.xticks(np.arange(4.5,9.5,.5))
-
-# detections over 6-min counts
-data.Minute.value_counts()
+## EXPORT
+birds.to_csv('E:\Share Drive\Farm Surveys\Raw Data\Spring 2020\countdata_cleaned.csv', index=False)
 
 
